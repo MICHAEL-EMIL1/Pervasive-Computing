bb8d631f46ece30c7b33d634d0f9abf67d44ebf8
michael <micheal.emil31@gmail.com>
2024-04-14 14:23:02 +0200

    modify forget password logic

--- app/src/main/java/com/example/pervasivecomputingproject/Forgetpass.java
+++ app/src/main/java/com/example/pervasivecomputingproject/Forgetpass.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,18 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Forgetpass extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgetpass);
-
+        EditText email = findViewById(R.id.forget_password_email);
         Button sendEmail=findViewById(R.id.forget_password) ;
         sendEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Forgetpass.this, ResetPassword.class));
+                String Email = email.getText().toString();
+                if (Email.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Missing Email.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Input is valid, proceed to home page
+                    Intent i = new Intent(Forgetpass.this, ResetPassword.class);
+                    startActivity(i);
+                    finish(); // Optionally finish SignIn activity to prevent going back when pressing back button
+                }
             }
         });
 
